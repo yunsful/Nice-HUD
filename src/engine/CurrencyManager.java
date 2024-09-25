b2a1d7e119ed94d3c89fc1795f95359b1e9bb0eb
Amarok121 <76926216+Amarok121@users.noreply.github.com>
2024-09-25 19:12:16 +0900

    add getcurrency, calculatecurrency

--- src/engine/CurrencyManager.java
+++ src/engine/CurrencyManager.java
@@ -2,6 +2,8 @@ package engine;
 
 import java.util.logging.Logger;
 
+
+
 public final class CurrencyManager {
     private static CurrencyManager instance;
     private static Logger logger;
@@ -20,5 +22,30 @@ public final class CurrencyManager {
         if (instance == null)
              instance = new CurrencyManager();
         return instance;
+    }
+
+
+    public int calculateCurrency(int score, float hitRate, int
+            clearTime, int maxTime) {
+        //
+
+        int currency = score / 10;
+        //
+
+        if (hitRate > 80.0) {
+            currency += (int) (currency * 0.2); // 20% 보너스 지급
+        }
+        //
+        
+        int timeBonus = (maxTime - clearTime) / 10;
+        if (timeBonus > 0) {
+            currency += timeBonus;
+        }
+        return currency;
+    }
+
+
+    public static int getCurrency() {
+        return FileManager.loadCurrency();
     }
 }

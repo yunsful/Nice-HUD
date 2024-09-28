82f64de1357c9c38ef37e822534723b58241eeb3
Suh Dong Jun <169052999+DongjunSuh923@users.noreply.github.com>
2024-09-29 00:48:44 +0900

    Commiting again due to Misupdate

--- src/clove/AchievementConditions.java
+++ src/clove/AchievementConditions.java
@@ -1,4 +1,15 @@
 package clove;
 
-public class AchievementConditions {
+public class AchievementConditions extends Achievement {
+    private static final int REQ_KILL_100 = 100;
+
+    public AchievementConditions() {
+        super("Space Hunter", "Defeat 100 enemies", REQ_KILL_100);
+    }
+
+    public void onkill(int currentKills) {
+        if (currentKills > REQ_KILL_100 && !isCompleted()) {
+            completeAchievement();
+        }
+    }
 }
--- src/clove/AchievementManager.java
+++ src/clove/AchievementManager.java
@@ -5,11 +5,12 @@ import java.util.HashMap;
 import java.util.Set;
 
 // TODO : Replace Object to Achievement Class
+// 수정 완료, 테스트 필요
 public class AchievementManager {
     /*
         Variables
      */
-    private HashMap<Object, Boolean> achievementMap;
+    private HashMap<Achievement, Boolean> achievementMap; // Object -> Achievement
     private ArrayList<AchievementChangedCallback> achievementChangedCallbacks;
 
     /*
@@ -17,7 +18,7 @@ public class AchievementManager {
      */
     @FunctionalInterface
     public interface AchievementChangedCallback {
-        void onAchievementChanged(Object achievement, boolean value);
+        void onAchievementChanged(Achievement achievement, boolean value); // Object -> Achievement
     }
 
     public void addAchievementChangedCallback(AchievementChangedCallback callback){
@@ -31,35 +32,39 @@ public class AchievementManager {
     /*
         Declare
      */
-    public AchievementManager(){
-        ;
+    public AchievementManager() { // HashMap<Achievement, Boolean>으로 초기화
+        achievementMap = new HashMap<>();
+        achievementChangedCallbacks = new ArrayList<>();
     }
 
     /*
         Functions
      */
-    public boolean AddAchievement(Object achievement, Boolean completed){
+
+    public boolean addAchievement(Achievement achievement, Boolean completed) { // Object -> Achievement
         if (achievementMap.containsKey(achievement))
-            return false; // 이미 업적이 추가되었을 경우 거절
+            return false;
         achievementMap.put(achievement, completed);
-        return true;
+        return true; // Changed code
     }
 
-    public boolean AddAchievement(Object achievement){
-        return AddAchievement(achievement, false);
+    public boolean addAchievement(Achievement achievement) { // Object -> Achievement로 변경
+        return addAchievement(achievement, false);
     }
 
-    public boolean hasAchivement(Object achievement){
+    public boolean hasAchivement(Achievement achievement){ // Object -> Achievement
         return achievementMap.containsKey(achievement);
     }
 
-    public Set<Object> getAchievements(){
+    public Set<Achievement> getAchievements() { // Object -> Achievement
         return achievementMap.keySet();
     }
 
-    public boolean setAchievementValue(Object achievement, Boolean completed){
+    public boolean setAchievementValue(Achievement achievement, Boolean completed) { // Object -> Achievement
         if(!hasAchivement(achievement)){
             // TODO : 업적을 설정하는 것에 실패했다는 알림(또는 로그) 출력
+            // 로그 출력 코드 작성 완료, 테스트 필요
+            System.out.println("Failed to set achievement: " + achievement.getAchievementName()); // 로그 추가
             return false;
         }
         achievementMap.replace(achievement, completed);
@@ -69,7 +74,7 @@ public class AchievementManager {
         return true;
     }
 
-    public boolean getAchievementValue(Object achievement){
+    public boolean getAchievementValue(Achievement achievement) { // Object -> Achievement
         // Boolean 타입을 boolean 타입으로 변환하여 반환
         return Boolean.TRUE.equals(achievementMap.getOrDefault(achievement, false));
     }
@@ -78,7 +83,11 @@ public class AchievementManager {
         편의성 함수
         (코드를 편하게 사용하기 위해 추가된 함수)
      */
-    public boolean completeAchievement(Object achievement){
-        return setAchievementValue(achievement, true);
+    public boolean completeAchievement(Achievement achievement) { // 코드 추가
+        if (!achievement.isCompleted()) {
+            achievement.completeAchievement();
+            return setAchievementValue(achievement, true);
+        }
+        return false;
     }
 }

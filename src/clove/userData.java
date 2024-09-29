9fa2ddf7692759dcd0385edd84efecf2569beb58
woojinss <swj12325@hanyang.ac.kr>
2024-09-29 14:34:34 +0900

    Reverted the mistake

--- src/clove/userData.java
+++ src/clove/userData.java
@@ -1,4 +1,33 @@
 package clove;
 
+//userDataSet
 public class userData {
+    private String userName;
+    private int killCount;
+    private int Level;
+    private int highScore;
+
+    public userData(){}
+
+    public userData(String userName, int killCount, int Level, int Score) {
+        this.userName = userName;
+        this.killCount = killCount;
+        this.Level = Level;
+        this.highScore = Score;
+    }
+
+    //userData Get and Set
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+
+    public int getKillCount() { return killCount; }
+    public void setKillCount(int killCount) { this.killCount = killCount; }
+
+    public int getLevel() { return Level; }
+    public void setLevel(int Level) { this.Level = Level; }
+
+    public int getHighScore() { return highScore; }
+    public void setHighScore(int highScore) { this.highScore = highScore; }
+
+
 }

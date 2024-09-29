e7b6eca4cca4bc0d0ba879029ec558c32cf7e8b9
ise-e <amsmam1234@gmail.com>
2024-09-29 16:57:31 +0900

    fix SetStaus_class to ShipStatus_file

--- src/inventory_develop/ShipStatus.java
+++ src/inventory_develop/ShipStatus.java
@@ -1,15 +1,18 @@
 package inventory_develop;
 
-import engine.DrawManager;
-
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class ShipStatus {
 
     protected int Speed;
     protected int SHOOTING_INTERVAL;
     protected int BULLET_SPEED;
+    protected int Speed_increase;
+    protected int SHOOTING_INTERVAL_increase;
+    protected int BULLET_SPEED_increase;
+
 
     Properties properties = new Properties();
 
@@ -23,27 +26,42 @@ public class ShipStatus {
 
     public void SetStatus(final int Speed_Level, final int SHOOTING_INTERVAL_Level,
                           final int BULLET_SPEED_Level) {
-        this.Speed += Speed_Level - 1;
-        this.SHOOTING_INTERVAL -= 100 * SHOOTING_INTERVAL_Level - 100;
-        this.BULLET_SPEED -= 2 * BULLET_SPEED_Level - 2;
+        this.Speed += Speed_increase * Speed_Level - Speed;
+        this.SHOOTING_INTERVAL += SHOOTING_INTERVAL_increase * SHOOTING_INTERVAL_Level - SHOOTING_INTERVAL;
+        this.BULLET_SPEED += BULLET_SPEED_increase * BULLET_SPEED_Level - BULLET_SPEED;
     }
 
-//    public void loadStatus() throws IOException{
-//        InputStream inputStream = null;
-//        try{
-//            inputStream = this.getClass().getClassLoader()
-//                    .getResourceAsStream("Status_increase");
-//            properties.load(inputStream);
-//            System.out.println(properties.getProperty("Speed.increase"));
-//            if (inputStream != null)
-//                inputStream.close();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("FileNotFound");
-//        } finally {
-//            if (inputStream != null)
-//                inputStream.close();
-//        }
-//    }
+
+    public void loadStatus(){
+        try (InputStream inputStream = ShipStatus.class.getClassLoader().getResourceAsStream("inventory_develop/Status_increase.properties")) {
+            if (inputStream == null) {
+                System.out.println("FileNotFound");
+                return;
+            }
+
+            properties.load(inputStream);
+
+            int statusValue = Integer.parseInt(properties.getProperty("Speed"));
+            Speed = statusValue;
+            statusValue = Integer.parseInt(properties.getProperty("Speed.increase"));
+            Speed_increase = statusValue;
+
+            statusValue = Integer.parseInt(properties.getProperty("SHOOTING_INTERVAL"));
+            SHOOTING_INTERVAL = statusValue;
+            statusValue = Integer.parseInt(properties.getProperty("SHOOTING_INTERVAL.increase"));
+            SHOOTING_INTERVAL_increase= statusValue;
+
+            statusValue = Integer.parseInt(properties.getProperty("BULLET_SPEED"));
+            BULLET_SPEED = statusValue;
+            statusValue = Integer.parseInt(properties.getProperty("BULLET_SPEED.increase"));
+            BULLET_SPEED_increase = statusValue;
+
+            System.out.println(this.SHOOTING_INTERVAL_increase);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public final int getSpeed(){

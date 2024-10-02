405d783ca3d984cbb312afb70aa71f664ec0ac7a
Koominjae <koom0406@naver.com>
2024-10-03 00:05:45 +0900

    Correct format

--- src/engine/FileManager.java
+++ src/engine/FileManager.java
@@ -27,254 +27,260 @@ import engine.DrawManager.SpriteType;
  * Manages files used in the application.
  *
  * @author <a href="mailto:RobertoIA1987@gmail.com">Roberto Izquierdo Amo</a>
+ *
  */
 public final class FileManager {
 
-    /**
-     * Singleton instance of the class.
-     */
-    private static FileManager instance;
-    /**
-     * Application logger.
-     */
-    private static Logger logger;
-    /**
-     * Max number of high scores.
-     */
-    private static final int MAX_SCORES = 7;
+	/** Singleton instance of the class. */
+	private static FileManager instance;
+	/** Application logger. */
+	private static Logger logger;
+	/** Max number of high scores. */
+	private static final int MAX_SCORES = 7;
 
-    /**
-     * private constructor.
-     */
-    private FileManager() {
-        logger = Core.getLogger();
-    }
+	/**
+	 * private constructor.
+	 */
+	private FileManager() {
+		logger = Core.getLogger();
+	}
 
-    /**
-     * Returns shared instance of FileManager.
-     *
-     * @return Shared instance of FileManager.
-     */
-    protected static FileManager getInstance() {
-        if (instance == null)
-            instance = new FileManager();
-        return instance;
-    }
+	/**
+	 * Returns shared instance of FileManager.
+	 *
+	 * @return Shared instance of FileManager.
+	 */
+	protected static FileManager getInstance() {
+		if (instance == null)
+			instance = new FileManager();
+		return instance;
+	}
 
-    /**
-     * Loads sprites from disk.
-     *
-     * @param spriteMap Mapping of sprite type and empty boolean matrix that will
-     *                  contain the image.
-     * @throws IOException In case of loading problems.
-     */
-    public void loadSprite(final Map<SpriteType, boolean[][]> spriteMap)
-            throws IOException {
-        InputStream inputStream = null;
+	/**
+	 * Loads sprites from disk.
+	 *
+	 * @param spriteMap
+	 *            Mapping of sprite type and empty boolean matrix that will
+	 *            contain the image.
+	 * @throws IOException
+	 *             In case of loading problems.
+	 */
+	public void loadSprite(final Map<SpriteType, boolean[][]> spriteMap)
+			throws IOException {
+		InputStream inputStream = null;
 
-        try {
-            inputStream = DrawManager.class.getClassLoader()
-                    .getResourceAsStream("graphics");
-            char c;
+		try {
+			inputStream = DrawManager.class.getClassLoader()
+					.getResourceAsStream("graphics");
+			char c;
 
-            // Sprite loading.
-            for (Map.Entry<SpriteType, boolean[][]> sprite : spriteMap
-                    .entrySet()) {
-                for (int i = 0; i < sprite.getValue().length; i++)
-                    for (int j = 0; j < sprite.getValue()[i].length; j++) {
-                        do
-                            c = (char) inputStream.read();
-                        while (c != '0' && c != '1');
+			// Sprite loading.
+			for (Map.Entry<SpriteType, boolean[][]> sprite : spriteMap
+					.entrySet()) {
+				for (int i = 0; i < sprite.getValue().length; i++)
+					for (int j = 0; j < sprite.getValue()[i].length; j++) {
+						do
+							c = (char) inputStream.read();
+						while (c != '0' && c != '1');
 
-                        if (c == '1')
-                            sprite.getValue()[i][j] = true;
-                        else
-                            sprite.getValue()[i][j] = false;
-                    }
-                logger.fine("Sprite " + sprite.getKey() + " loaded.");
-            }
-            if (inputStream != null)
-                inputStream.close();
-        } finally {
-            if (inputStream != null)
-                inputStream.close();
-        }
-    }
+						if (c == '1')
+							sprite.getValue()[i][j] = true;
+						else
+							sprite.getValue()[i][j] = false;
+					}
+				logger.fine("Sprite " + sprite.getKey() + " loaded.");
+			}
+			if (inputStream != null)
+				inputStream.close();
+		} finally {
+			if (inputStream != null)
+				inputStream.close();
+		}
+	}
 
-    /**
-     * Loads a font of a given size.
-     *
-     * @param size Point size of the font.
-     * @return New font.
-     * @throws IOException         In case of loading problems.
-     * @throws FontFormatException In case of incorrect font format.
-     */
-    public Font loadFont(final float size) throws IOException,
-            FontFormatException {
-        InputStream inputStream = null;
-        Font font;
+	/**
+	 * Loads a font of a given size.
+	 *
+	 * @param size
+	 *            Point size of the font.
+	 * @return New font.
+	 * @throws IOException
+	 *             In case of loading problems.
+	 * @throws FontFormatException
+	 *             In case of incorrect font format.
+	 */
+	public Font loadFont(final float size) throws IOException,
+			FontFormatException {
+		InputStream inputStream = null;
+		Font font;
 
-        try {
-            inputStream = FileManager.class.getClassLoader()
-                    .getResourceAsStream("font.ttf");
-            if (inputStream != null) {
-                font = Font.createFont(Font.TRUETYPE_FONT, inputStream).deriveFont(size);
-            } else {
-                // Set as default font, if inputStream is null
-                System.out.println("Custom font not found, applying default font.");
-                font = new Font("Serif", Font.PLAIN, (int) size); // Set as "Serif"
-            }
-        } finally {
-            if (inputStream != null)
-                inputStream.close();
-        }
+		try {
+			inputStream = FileManager.class.getClassLoader()
+					.getResourceAsStream("font.ttf");
+			if (inputStream != null) {
+				font = Font.createFont(Font.TRUETYPE_FONT, inputStream).deriveFont(size);
+			} else {
+				// Set as default font, if inputStream is null
+				System.out.println("Custom font not found, applying default font.");
+				font = new Font("Serif", Font.PLAIN, (int) size); // Set as "Serif"
+			}
+		} finally {
+			if (inputStream != null)
+				inputStream.close();
+		}
 
-        return font;
-    }
+		return font;
+	}
 
-    /**
-     * Returns the application default scores if there is no user high scores
-     * file.
-     *
-     * @return Default high scores.
-     * @throws IOException In case of loading problems.
-     */
-    private List<Score> loadDefaultHighScores() throws IOException {
-        List<Score> highScores = new ArrayList<Score>();
-        InputStream inputStream = null;
-        BufferedReader reader = null;
+	/**
+	 * Returns the application default scores if there is no user high scores
+	 * file.
+	 *
+	 * @return Default high scores.
+	 * @throws IOException
+	 *             In case of loading problems.
+	 */
+	private List<Score> loadDefaultHighScores() throws IOException {
+		List<Score> highScores = new ArrayList<Score>();
+		InputStream inputStream = null;
+		BufferedReader reader = null;
 
-        try {
-            inputStream = FileManager.class.getClassLoader()
-                    .getResourceAsStream("scores");
-            reader = new BufferedReader(new InputStreamReader(inputStream));
+		try {
+			inputStream = FileManager.class.getClassLoader()
+					.getResourceAsStream("scores");
+			reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            Score highScore = null;
-            String name = reader.readLine();
-            String score = reader.readLine();
+			Score highScore = null;
+			String name = reader.readLine();
+			String score = reader.readLine();
 
-            while ((name != null) && (score != null)) {
-                highScore = new Score(name, Integer.parseInt(score));
-                highScores.add(highScore);
-                name = reader.readLine();
-                score = reader.readLine();
-            }
-        } finally {
-            if (inputStream != null)
-                inputStream.close();
-        }
+			while ((name != null) && (score != null)) {
+				highScore = new Score(name, Integer.parseInt(score));
+				highScores.add(highScore);
+				name = reader.readLine();
+				score = reader.readLine();
+			}
+		} finally {
+			if (inputStream != null)
+				inputStream.close();
+		}
 
-        return highScores;
-    }
+		return highScores;
+	}
 
-    /**
-     * Loads high scores from file, and returns a sorted list of pairs score -
-     * value.
-     *
-     * @return Sorted list of scores - players.
-     * @throws IOException In case of loading problems.
-     */
-    public List<Score> loadHighScores() throws IOException {
+	/**
+	 * Loads high scores from file, and returns a sorted list of pairs score -
+	 * value.
+	 *
+	 * @return Sorted list of scores - players.
+	 * @throws IOException
+	 *             In case of loading problems.
+	 */
+	public List<Score> loadHighScores() throws IOException {
 
-        List<Score> highScores = new ArrayList<Score>();
-        InputStream inputStream = null;
-        BufferedReader bufferedReader = null;
+		List<Score> highScores = new ArrayList<Score>();
+		InputStream inputStream = null;
+		BufferedReader bufferedReader = null;
 
-        try {
-            String jarPath = FileManager.class.getProtectionDomain()
-                    .getCodeSource().getLocation().getPath();
-            jarPath = URLDecoder.decode(jarPath, "UTF-8");
+		try {
+			String jarPath = FileManager.class.getProtectionDomain()
+					.getCodeSource().getLocation().getPath();
+			jarPath = URLDecoder.decode(jarPath, "UTF-8");
 
-            String scoresPath = new File(jarPath).getParent();
-            scoresPath += File.separator;
-            scoresPath += "scores";
+			String scoresPath = new File(jarPath).getParent();
+			scoresPath += File.separator;
+			scoresPath += "scores";
 
-            File scoresFile = new File(scoresPath);
-            inputStream = new FileInputStream(scoresFile);
-            bufferedReader = new BufferedReader(new InputStreamReader(
-                    inputStream, Charset.forName("UTF-8")));
+			File scoresFile = new File(scoresPath);
+			inputStream = new FileInputStream(scoresFile);
+			bufferedReader = new BufferedReader(new InputStreamReader(
+					inputStream, Charset.forName("UTF-8")));
 
-            logger.info("Loading user high scores.");
+			logger.info("Loading user high scores.");
 
-            Score highScore = null;
-            String name = bufferedReader.readLine();
-            String score = bufferedReader.readLine();
+			Score highScore = null;
+			String name = bufferedReader.readLine();
+			String score = bufferedReader.readLine();
 
-            while ((name != null) && (score != null)) {
-                highScore = new Score(name, Integer.parseInt(score));
-                highScores.add(highScore);
-                name = bufferedReader.readLine();
-                score = bufferedReader.readLine();
-            }
+			while ((name != null) && (score != null)) {
+				highScore = new Score(name, Integer.parseInt(score));
+				highScores.add(highScore);
+				name = bufferedReader.readLine();
+				score = bufferedReader.readLine();
+			}
 
-        } catch (FileNotFoundException e) {
-            // loads default if there's no user scores.
-            logger.info("Loading default high scores.");
-            highScores = loadDefaultHighScores();
-        } finally {
-            if (bufferedReader != null)
-                bufferedReader.close();
-        }
+		} catch (FileNotFoundException e) {
+			// loads default if there's no user scores.
+			logger.info("Loading default high scores.");
+			highScores = loadDefaultHighScores();
+		} finally {
+			if (bufferedReader != null)
+				bufferedReader.close();
+		}
 
-        Collections.sort(highScores);
-        return highScores;
-    }
+		Collections.sort(highScores);
+		return highScores;
+	}
 
-    /**
-     * Saves user high scores to disk.
-     *
-     * @param highScores High scores to save.
-     * @throws IOException In case of loading problems.
-     */
-    public void saveHighScores(final List<Score> highScores)
-            throws IOException {
-        OutputStream outputStream = null;
-        BufferedWriter bufferedWriter = null;
+	/**
+	 * Saves user high scores to disk.
+	 *
+	 * @param highScores
+	 *            High scores to save.
+	 * @throws IOException
+	 *             In case of loading problems.
+	 */
+	public void saveHighScores(final List<Score> highScores)
+			throws IOException {
+		OutputStream outputStream = null;
+		BufferedWriter bufferedWriter = null;
 
-        try {
-            String jarPath = FileManager.class.getProtectionDomain()
-                    .getCodeSource().getLocation().getPath();
-            jarPath = URLDecoder.decode(jarPath, "UTF-8");
+		try {
+			String jarPath = FileManager.class.getProtectionDomain()
+					.getCodeSource().getLocation().getPath();
+			jarPath = URLDecoder.decode(jarPath, "UTF-8");
 
-            String scoresPath = new File(jarPath).getParent();
-            scoresPath += File.separator;
-            scoresPath += "scores";
+			String scoresPath = new File(jarPath).getParent();
+			scoresPath += File.separator;
+			scoresPath += "scores";
 
-            File scoresFile = new File(scoresPath);
+			File scoresFile = new File(scoresPath);
 
-            if (!scoresFile.exists())
-                scoresFile.createNewFile();
+			if (!scoresFile.exists())
+				scoresFile.createNewFile();
 
-            outputStream = new FileOutputStream(scoresFile);
-            bufferedWriter = new BufferedWriter(new OutputStreamWriter(
-                    outputStream, Charset.forName("UTF-8")));
+			outputStream = new FileOutputStream(scoresFile);
+			bufferedWriter = new BufferedWriter(new OutputStreamWriter(
+					outputStream, Charset.forName("UTF-8")));
 
-            logger.info("Saving user high scores.");
+			logger.info("Saving user high scores.");
 
-            // Saves 7 or less scores.
-            int savedCount = 0;
-            for (Score score : highScores) {
-                if (savedCount >= MAX_SCORES)
-                    break;
-                bufferedWriter.write(score.getName());
-                bufferedWriter.newLine();
-                bufferedWriter.write(Integer.toString(score.getScore()));
-                bufferedWriter.newLine();
-                savedCount++;
-            }
+			// Saves 7 or less scores.
+			int savedCount = 0;
+			for (Score score : highScores) {
+				if (savedCount >= MAX_SCORES)
+					break;
+				bufferedWriter.write(score.getName());
+				bufferedWriter.newLine();
+				bufferedWriter.write(Integer.toString(score.getScore()));
+				bufferedWriter.newLine();
+				savedCount++;
+			}
 
-        } finally {
-            if (bufferedWriter != null)
-                bufferedWriter.close();
-        }
-    }
+		} finally {
+			if (bufferedWriter != null)
+				bufferedWriter.close();
+		}
+	}
 
-    /**
-     * Saves user currency to disk.
-     *
-     * @param currency amount of user currency to save.
-     * @throws IOException In case of saving problems.
-     */
+	/**
+	 * Saves user currency to disk.
+	 *
+	 * @param currency
+	 *            amount of user currency to save.
+	 * @throws IOException
+	 *             In case of saving problems.
+	 */
 
     public void saveCurrency(final int currency) throws IOException {
         InputStream inputStream = null;
@@ -297,16 +303,16 @@ public final class FileManager {
             if (!currencyFile.exists())
                 currencyFile.createNewFile();
 
-            // Read the file's current content
             List<String> lines = new ArrayList<>();
-            inputStream = new FileInputStream(currencyFile);
-            outputStream = new FileOutputStream(currencyFile);
-            bufferedWriter = new BufferedWriter(new OutputStreamWriter(
+			inputStream = new FileInputStream(currencyFile);
+			outputStream = new FileOutputStream(currencyFile);
+			bufferedWriter = new BufferedWriter(new OutputStreamWriter(
                     outputStream, Charset.forName("UTF-8")));
-            bufferedReader = new BufferedReader(new InputStreamReader(
+			bufferedReader = new BufferedReader(new InputStreamReader(
                     inputStream, Charset.forName("UTF-8")));
 
-            String line;
+			// Read the file's current content
+			String line;
             while ((line = bufferedReader.readLine()) != null) {
                 lines.add(line);
             }
@@ -337,12 +343,13 @@ public final class FileManager {
         }
     }
 
-    /**
-     * Loads user currency from file, and returns current currency.
-     *
-     * @return amount of current currency.
-     * @throws IOException In case of loading problems.
-     */
+	/**
+	 * Loads user currency from file, and returns current currency.
+	 *
+	 * @return amount of current currency.
+	 * @throws IOException
+	 *             In case of loading problems.
+	 */
     public int loadCurrency() throws IOException {
         int currency;
         InputStream inputStream = null;
@@ -378,12 +385,13 @@ public final class FileManager {
         return currency;
     }
 
-    /**
-     * Returns the application default currency if there is no user currency files.
-     *
-     * @return Default currency.
-     * @throws IOException In case of loading problems.
-     */
+	/**
+	 * Returns the application default currency if there is no user currency files.
+	 *
+	 * @return Default currency.
+	 * @throws IOException
+	 *             In case of loading problems.
+	 */
     private int loadDefaultCurrency() throws IOException {
         int currency;
         InputStream inputStream = null;
@@ -407,8 +415,10 @@ public final class FileManager {
     /**
      * Saves user gem to disk.
      *
-     * @param gem amount of user gem to save.
-     * @throws IOException In case of saving problems.
+     * @param gem
+	 *            amount of user gem to save.
+     * @throws IOException
+	 * 			   In case of saving problems.
      */
     // Team-Ctrl-S(Currency)
     public void saveGem(final int gem) throws IOException {
@@ -476,7 +486,8 @@ public final class FileManager {
      * Loads user gem from file, and returns current gem.
      *
      * @return amount of current gem.
-     * @throws IOException In case of loading problems.
+     * @throws IOException
+	 * 			   In case of loading problems.
      */
     // Team-Ctrl-S(Currency)
     public int loadGem() throws IOException {
@@ -519,7 +530,8 @@ public final class FileManager {
      * Returns the application default gem if there is no user gem files.
      *
      * @return Default gem.
-     * @throws IOException In case of loading problems.
+     * @throws IOException
+	 * 			   In case of loading problems.
      */
     // Team-Ctrl-S(Currency)
     private int loadDefaultGem() throws IOException {

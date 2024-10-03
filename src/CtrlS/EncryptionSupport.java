0b861cf6cd72cc1ad99b406e99ff643fa10c34a0
Uichan Jung <jchan3214@gmail.com>
2024-10-03 18:08:20 +0900

    Add EncryptionSupport class to CtrlS

---
+++ src/CtrlS/EncryptionSupport.java
@@ -0,0 +1,58 @@
+package CtrlS;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public final class EncryptionSupport {
+
+    public KeyPair genRSAKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+        gen.initialize(1024, new SecureRandom());
+        return gen.genKeyPair();
+    }
+
+    public String encryptRSA(String plainText, PublicKey publicKey)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        byte[] bytePlain = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(bytePlain);
+    }
+
+    public String decryptRSA(String encrypted, PrivateKey privateKey)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        byte[] byteEncrypted = Base64.getDecoder().decode(encrypted.getBytes());
+
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] bytePlain = cipher.doFinal(byteEncrypted);
+        return new String(bytePlain, "utf-8");
+    }
+
+    public PublicKey getPublicKeyFromBase64Encrypted(String base64PublicKey)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] decodedBase64PubKey = Base64.getDecoder().decode(base64PublicKey);
+
+        return KeyFactory.getInstance("RSA")
+                .generatePublic(new X509EncodedKeySpec(decodedBase64PubKey));
+    }
+
+    public PrivateKey getPrivateKeyFromBase64Encrypted(String base64PrivateKey)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] decodedBase64PrivateKey = Base64.getDecoder().decode(base64PrivateKey);
+
+        return KeyFactory.getInstance("RSA")
+                .generatePrivate(new PKCS8EncodedKeySpec(decodedBase64PrivateKey));
+    }
+}

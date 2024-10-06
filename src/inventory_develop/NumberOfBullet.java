a46a88ed74a4d588899239b88a848ee28f462ad5
ise-e <amsmam1234@hanyang.ac.kr>
2024-10-06 17:12:00 +0900

    Apply items to actual games

--- src/inventory_develop/NumberOfBullet.java
+++ src/inventory_develop/NumberOfBullet.java
@@ -1,8 +1,7 @@
 package inventory_develop;
 
-import entity.Bullet;
-import entity.BulletPool;
-import entity.Entity;
+import Enemy.PiercingBullet;
+import Enemy.PiercingBulletPool;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,9 +16,10 @@ public class NumberOfBullet{
     private static final int OFFSET_X_THREEBULLETS = 12;
 
     /** Bullet levels */
-    private static int BulletLevel = 2;
-
-    private BulletPool bulletPool;
+    private static int bulletLevel = 1;
+    /** PiercingBullet levles */
+    private static int piercingbulletLevel = 1;
+    private final int PierceMax = 3;
 
     /**
      * Constructor
@@ -31,66 +31,43 @@ public class NumberOfBullet{
      *
      * @return
      */
-    public Set<Bullet> AddBullet(int positionX, int positionY, int speed) {
-        Set<Bullet> bullets = new HashSet<>();
+    public Set<PiercingBullet> addBullet(int positionX, int positionY, int speed, boolean canShootBomb) {
+        Set<PiercingBullet> bullets = new HashSet<>();
 
-        switch (BulletLevel) {
+        if (canShootBomb) {
+            bullets.add(PiercingBulletPool.getPiercingBullet(positionX, positionY, speed, 1));
+            return bullets;
+        }
+
+        switch (bulletLevel) {
             case 1:
-                bullets.add(bulletPool.getBullet(positionX, positionY, speed));
+                bullets.add(PiercingBulletPool.getPiercingBullet(positionX, positionY, speed, piercingbulletLevel));
                 break;
             case 2:
-                bullets.add(bulletPool.getBullet(positionX - OFFSET_X_TWOBULLETS, positionY, speed));
-                bullets.add(bulletPool.getBullet(positionX + OFFSET_X_TWOBULLETS, positionY, speed));
+                bullets.add(PiercingBulletPool.getPiercingBullet(positionX - OFFSET_X_TWOBULLETS + 5, positionY, speed, piercingbulletLevel));
+                bullets.add(PiercingBulletPool.getPiercingBullet(positionX + OFFSET_X_TWOBULLETS - 5, positionY, speed, piercingbulletLevel));
                 break;
             case 3:
-                bullets.add(bulletPool.getBullet(positionX + OFFSET_X_TWOBULLETS, positionY, speed));
-                bullets.add(bulletPool.getBullet(positionX, positionY, speed));
-                bullets.add(bulletPool.getBullet(positionX - OFFSET_X_TWOBULLETS, positionY, speed));
+                bullets.add(PiercingBulletPool.getPiercingBullet(positionX + OFFSET_X_THREEBULLETS, positionY, speed, piercingbulletLevel));
+                bullets.add(PiercingBulletPool.getPiercingBullet(positionX, positionY, speed,piercingbulletLevel));
+                bullets.add(PiercingBulletPool.getPiercingBullet(positionX - OFFSET_X_THREEBULLETS, positionY, speed, piercingbulletLevel));
                 break;
         }
+
         return bullets;
 
     }
 
-
-
-    /**
-     * Returns two bullets from the pool if available, otherwise creates new ones.
-     * Ensures the bullets are placed at different positions.
-     *
-     * @param positionX Requested position of the bullet in the X axis.
-     * @param positionY Requested position of the bullet in the Y axis.
-     * @param speed     Requested speed of the bullet, positive or negative depending
-     *                  on direction - positive is down.
-     * @return An array containing two bullets.
-     */
-    public Bullet[] getTwoBullets(int positionX, int positionY, int speed) {
-        Bullet[] bullets = new Bullet[2];
-
-        bullets[0] = bulletPool.getBullet(positionX - OFFSET_X_TWOBULLETS, positionY, speed);
-        bullets[1] = bulletPool.getBullet(positionX + OFFSET_X_TWOBULLETS, positionY, speed);
-
-        return bullets;
+    public void bulletup(){
+        if (bulletLevel <= 3){
+            bulletLevel += 1;
+        }
     }
 
-    /**
-     * Returns three bullets from the pool if available, otherwise creates new ones.
-     * Ensures the bullets are placed at different positions.
-     *
-     * @param positionX Requested position of the bullet in the X axis.
-     * @param positionY Requested position of the bullet in the Y axis.
-     * @param speed     Requested speed of the bullet, positive or negative depending
-     *                  on direction - positive is down.
-     * @return An array containing three bullets.
-     */
-    public Bullet[] getThreeBullets(int positionX, int positionY, int speed) {
-        Bullet[] bullets = new Bullet[3];
-
-        bullets[0] = bulletPool.getBullet(positionX - OFFSET_X_THREEBULLETS, positionY, speed);
-        bullets[1] = bulletPool.getBullet(positionX, positionY, speed);
-        bullets[2] = bulletPool.getBullet(positionX + OFFSET_X_THREEBULLETS, positionY, speed);
-
-        return bullets;
+    public void pierceup() {
+        if (piercingbulletLevel < PierceMax){
+            piercingbulletLevel += 1;
+        }
     }
 
 }

85f59ff98bd8b79cab1ebd0e74d79972f6a128fc
ise-e <162322609+ise-e@users.noreply.github.com>
2024-10-13 15:20:42 +0900

    Revert "Revert "Feature/item fever""

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -98,14 +98,16 @@ public class DrawManager {
 		Boss, //by enemy team
 		/** Player Lives. */
 		/** Item */
-    	ItemHeart,
+    ItemHeart,
 		ShipBarrierStatus,
 		ItemCoin,
 		ItemPierce,
 		ItemBomb,
 		ItemBarrier,
+		ItemFeverTime,
+		//Produced by Starter Team
 
-        //Produced by Starter Team
+    //Produced by Starter Team
 		/** coin */
 		Coin,
 		/** add sign */
@@ -154,6 +156,7 @@ public class DrawManager {
 			spriteMap.put(SpriteType.ItemBomb, new boolean[7][9]);
 			spriteMap.put(SpriteType.ShipBarrierStatus, new boolean[13][8]);	// temporary
 			spriteMap.put(SpriteType.ItemCoin, new boolean[7][7]);
+			spriteMap.put(SpriteType.ItemFeverTime, new boolean[9][9]);
 			spriteMap.put(SpriteType.ItemPierce, new boolean[7][7]);
 
 			fileManager.loadSprite(spriteMap);
